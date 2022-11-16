3362590d5ce90d24cb412da7609e80b71ad91561
ici <ici@DESKTOP-KN4UNKN>
2022-11-16 16:00:09 +0900

    오라클에서 회원 명부 불러오기

--- src/main/java/com/pengsoo/test/TestController.java
+++ src/main/java/com/pengsoo/test/TestController.java
@@ -1,5 +1,7 @@
 package com.pengsoo.test;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pengsoo.test.dao.IDao;
+import com.pengsoo.test.dto.MemberDto;
 
 @Controller
 public class TestController {
@@ -16,20 +19,20 @@ public class TestController {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@RequestMapping(value="/")
-	public String goList() {
-		return "redirect:join";
-	}
+//	@RequestMapping(value="/")
+//	public String goList() {
+//		return "redirect:join";
+//	}
 
 	@RequestMapping(value = "join")//join 화면만 보여준다.
 	public String join() {
 		
 		return "join";
 	}
-	
+	                                                                                                                                                                                                                                                                                 
 	@RequestMapping(value = "joinMember")//파라미터보내준다.
 	public String joinMember(HttpServletRequest request, Model model) {
-		
+		                                                            
 		IDao dao = sqlSession.getMapper(IDao.class);//IDao로 반환
 		
 		String mid = request.getParameter("mid");
@@ -44,5 +47,19 @@ public class TestController {
 		return "joinOk";
 	}
 	
+	@RequestMapping("memberList")
+	public String membeList(Model model) {
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		ArrayList<MemberDto> dtos = dao.memberList();//dtos=적당히 작명 dtos에 저장 통째로 보낸다
+		
+		//memberDtos.get(0).getMname();//첫번째 가입회원의 이름 추출
+		
+	    model.addAttribute("memberList", dtos);//dtos에 통째 실어서 보낸다
+
+		
+		return "memberList";
+	}
 	
 }
